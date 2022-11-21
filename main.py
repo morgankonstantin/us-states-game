1de418115680e9b3020af118068fa196fd7dbbb4
Morgan Konstantin <konstantin.morgan@gmail.com>
2022-11-21 09:43:18 +0100

    smol tweaks

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-correct_ans = 0  # Track correct guesses
-
 data = pd.read_csv("50_states.csv")
 states_list = data.state.to_list()
 guessed_states = []
@@ -18,15 +16,15 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state name?").title()  # Convert the input to title case
+
     if answer_state == "Exit":
         missing_states = [state for state in states_list if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)  # Convert list to DataFrame
         new_data.to_csv("states_to_learn")  # Create a new csv with the missed states
-        break
+
     # Check for the answer in the states_list, place label on the map if the guess is correct
     if answer_state in states_list:
         guessed_states.append(answer_state)
-        states_list.remove(answer_state)
         new_label = turtle.Turtle()  # Create a new turtle object
         new_label.hideturtle()
         new_label.pu()  # penup() so it won't draw any line
